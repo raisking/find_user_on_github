4fde65d63498d6dae4a7c44861b07ffdfc5b1d41
Prakash Rai <raiprakas007@gmail.com>
2018-01-11 22:52:20 -0500

    able to pull data from github

--- js/main.js
+++ js/main.js
@@ -10,7 +10,51 @@ $(document).ready(function(){
                 client_secret: '6973d5fa6672ac59142d3869f543e8493402e1ec'
             }
         }).done(function(user){
+            $.ajax({
+                url: 'https://api.github.com/users/' + username + '/repos',
+                data: {
+                    client_id: '1178504a091b4bbe37e9',
+                    client_secret: '6973d5fa6672ac59142d3869f543e8493402e1ec'
+                }
+            }).done(function(repos){
+               $.each(repos, function(index, repo){
+                   $('#repos').append(`
+                    <div class
+                   `);
+               })
+            })
+            $('#profile').html(`
+<div class="panel panel-default">
+  <div class="panel-heading">
+    <h3 class="panel-title">${user.name}</h3>
+  </div>
+  <div class="panel-body">
+    <div class ="row">
+            <div class="col-md-3">
+            <img class="thumbnail avatar" src="${user.avatar_url}">
+            <a target="_blank" class="btn btn-primary btn-block" href="${user.html_url}">View Profile</a>
+            </div>
+            <div class="col-md-9">
+            <span class="label label-default">Public Repos: ${user.public_repos}</span>
+            <span class="label label-primary">Public Gists: ${user.public_gists}</span>
+            <span class="label label-success">Followers: ${user.followers}</span>
+            <span class="label label-info">Following: ${user.following}</span>
+            <br>
+            <ul class = "list-group">
+                <li class ="list-group-item">Company: ${user.company}</li>
+                <li class ="list-group-item">Blog: ${user.blog}</li>
+                <li class ="list-group-item">Location: ${user.location}</li>
+                <li class ="list-group-item">Member Since: ${user.created_at}</li>
+                <li class ="list-group-item">Repos URL: ${user.repos_url}</li>
+            </ul>
+            </div>
             
+    </div>
+  </div>
+</div>
+<h3 class ="page-header">Lastest Repos</h3>
+            <div id ="repos"></div>
+            `);
         });
     });
 
